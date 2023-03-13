1e19e8b98556be5a7042ff8eb6acafd84c3aa043
LaasYath <67027434+LaasYath@users.noreply.github.com>
2023-03-13 16:05:40 -0500

    Add Event Feature Updated (still unable to reload getDates() after event added, basically can't see new event although created)

--- components/CalendarScreen.js
+++ components/CalendarScreen.js
@@ -1,10 +1,37 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
-import { Card, ActivityIndicator, Text, Divider } from 'react-native-paper';
+import { StyleSheet, View, ScrollView, RefreshControl } from 'react-native';
+import { Card, ActivityIndicator, Text, Divider, Button, Modal, Portal, TextInput } from 'react-native-paper';
 import CalendarPicker from 'react-native-calendar-picker';
 import moment from 'moment';
+import Storage from 'react-native-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // https://github.com/stephy/CalendarPicker
+// https://github.com/sunnylqm/react-native-storage
+
+//configure local storage for events
+let storage = new Storage({
+  // maximum capacity, default 1000 key-ids
+  size: 1000,
+
+  storageBackend: AsyncStorage, 
+
+  // null (never expires)
+  defaultExpires: null,
+
+  // cache data in the memory. default is true.
+  enableCache: true,
+
+  // if data was not found in storage or expired data was found,
+  // the corresponding sync method will be invoked returning
+  // the latest data.
+  sync: {
+    
+  }
+});
+
+export default storage;
+
 
 export const CalendarScreen = (props) => {
   // controls highlighting of calendar
@@ -73,6 +100,20 @@ export const CalendarScreen = (props) => {
 
       <Divider style={styles.divider} />
 
+      {/* <Button 
+        mode={'text'} 
+        onPress={addEvent()} 
+        style={styles.addEventButton} 
+      >
+        <Text 
+          style={styles.addEventText}
+        >
+          +
+        </Text>
+      </Button> */}
+      
+      <NewEventComponent />
+
       <ScrollView>
         {dateCards}
       </ScrollView>
@@ -99,8 +140,56 @@ const DateCard = props => {
   )
 }
 
-/**
- * TODO: Implement async getDates()
+//Event class to store user's events locally instead of flooding database
+class Events {
+  constructor(date, src, title, desc) {
+    this.date = date;
+    this.src = src;
+    this.title = title;
+    this.desc = desc;
+  }
+
+  // getDate() {
+  //   return this.date;
+  // }
+
+  // getSrc() {
+  //   return this.src;
+  // }
+
+  // getTitle() {
+  //   return this.title;
+  // }
+
+  // getDesc() {
+  //   return this.desc;
+  // }
+
+  getData() {
+    return {
+      date: this.date,
+      src: this.src,
+      title: this.title,
+      desc: this.desc
+    }
+  }
+}
+
+
+//dummy data (preexisting events)
+let event1 = new Events("20230310", "Chess Club", "Chess Comp", "At 2:00 pm in the main building")
+let event2 = new Events("20230322", "FBLA Club", "State Conference", "Compete to win the glory of being the best in the state!")
+let events = [event1.getData(), event2.getData()];
+storage.save({
+  key: 'personalEvents', 
+  data: {
+    addedEvents: events
+  },
+
+  expires: null
+});
+
+/*
  * 
  * Should return ALL dates linked to this account
  *   (aka all dates in all the clubs linked to this account + any school dates)
@@ -113,45 +202,204 @@ const DateCard = props => {
  *   desc: str,
  * }, ...
  * ]
- * 
- * date should be in YYYYMMDD format
- * ex: March 10th, 2023 would be 20230310
- * 
- * src should be where event is coming from 
- *  (ex: Chess club, school, etc.)
- * title is name of event
- * desc is description of event
- *  (just put time of event in desc)
  */
 const getDates = async () => {
-  const ret = [
-    {
-      date: '20230310',
-      src: 'Chess Club',
-      title: 'Chess Competition',
-      desc: 'At 2:00 pm in the main building',
-    },
-    {
-      date: '20230323',
-      src: 'FBLA',
-      title: 'State Competition',
-      desc: 'Compete to win the glory of being the best in the state!',
-    },
-    {
-      date: '20230323',
-      src: 'School',
-      title: 'Eat tacos',
-      desc: 'Come eat tacos for free at 10 AM in the side building!',
-    },
-    {
-      date: '20230323',
-      src: 'Robotics',
-      title: 'Lorem ipsum',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis a condimentum vitae sapien pellentesque. Dolor sit amet consectetur adipiscing.',
-    }
-  ];
+  // load
+  let listEvents = [];
+  storage
+  .load({
+    key: 'personalEvents',
 
-  return new Promise((res) => {setTimeout(() => res(ret), 3000)});
+    autoSync: true,
+
+    syncInBackground: true,
+
+    // syncParams: {
+    //   extraFetchOptions: {
+    //     
+    //   },
+    //   someFlag: true
+    // }
+  })
+  .then(ret => {
+    // found data go to then()
+    for (const object of ret.addedEvents) {
+      listEvents.push(object);
+    }
+  })
+  .catch(err => {
+    // any exception including data not found
+    // goes to catch()
+    console.warn(err.message);
+    switch (err.name) {
+      case 'NotFoundError':
+        // TODO;
+        break;
+      case 'ExpiredError':
+        // TODO
+        break;
+    }
+  });
+
+  return new Promise((res) => {setTimeout(() => res(listEvents), 3000)});
+}
+
+const NewEventComponent = props => {
+  const [visible, setVisible] = useState(false);
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+
+  return (
+    <View>
+      <Portal>
+        <NewEventModal visible={visible} onDismiss={hideModal} />
+      </Portal>
+      <Button 
+        mode={'text'} 
+        onPress={showModal} 
+        style={[styles.addEventButton]} 
+        contentStyle={styles.addEventText}
+      >
+        <Text>
+          +
+        </Text>
+      </Button>
+    </View>
+  );
+}
+
+const NewEventModal = props => {
+  const visible = props.visible;
+  const hideModal = props.onDismiss;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  //set date to current date
+  let date = new Date();
+  let month = date.getMonth()+1;
+  if (month < 10) {
+    month = "" + 0 + month;
+  }
+  let [dateTxt, setDateTxt] = useState(date.getFullYear() + "-" + month + "-" + date.getDate());
+
+  const [titleTxt, setTitleTxt] = useState("");
+  const [descTxt, setDescTxt] = useState("");
+  const [srcTxt, setSrcTxt] = useState("");
+  const [errorText, setErrorText] = useState("");
+
+  // TODO: Implement createNewAccount function
+  const createNewEvent = async () => {
+    const resetButton = () => {
+      setIsLoading(false);
+    }
+
+    setIsLoading(true);
+
+    await new Promise(res => setTimeout(res, 2000));
+
+    //error cases
+    if (!dateTxt || !titleTxt || !srcTxt || !descTxt) {
+      console.log('empty field error');
+      setErrorText('Please fill in all fields');
+      resetButton();
+      return;
+    }
+    
+    (async () => {
+      let dateOfEvent = dateTxt.replace(/-/g, "");
+      let newEvent = new Events(dateOfEvent, srcTxt, titleTxt, descTxt); //date, src, title, desc
+      //automatically change local storage data
+      events.push(newEvent.getData());
+      console.log(events);
+      storage.save({
+        key: 'personalEvents', 
+        data: {
+          addedEvents: events
+        },
+      
+        expires: null
+      });
+
+    })();
+
+    setErrorText('');
+    hideModal();
+    resetButton();
+    return;
+  }
+
+  // reset error text every time
+  // the modal is opened/closed
+  useEffect(() => {
+    setErrorText("");
+  }, [visible])
+
+  return (
+    <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={styles.modalStyle}>
+      <Text style={styles.title}>
+        New Event
+      </Text>
+      <TextInput style={styles.textInput}
+        label="Title"
+        value={titleTxt}
+        onChangeText={text => setTitleTxt(sanitize(text))}
+      />
+      <TextInput style={styles.textInput}
+        label="Group"
+        value={srcTxt}
+        onChangeText={text => setSrcTxt(sanitize(text))}
+      />
+      <TextInput style={styles.textInput}
+        label="Date (YYYY-MM-DD)"
+        value={dateTxt}
+        onChangeText={text => setDateTxt(sanitize(text))}
+      />
+      <TextInput style={styles.textInput}
+        label="Description"
+        value={descTxt}
+        onChangeText={text => setDescTxt(sanitize(text))}
+      />
+      <View>
+        <Text style={{ color: 'red' }}>
+          {errorText}
+        </Text>
+      </View>
+      <View style={styles.button}>
+        <NewEventButton 
+          onPress={() => { createNewEvent(); }} 
+          loading={isLoading}
+        />
+      </View>
+    </Modal>
+  );
+}
+
+
+const NewEventButton = props => (
+
+  <Button
+    loading={props.loading}
+    mode={'contained'}
+    onPress={props.onPress}
+  >
+    Create New Event
+  </Button>
+);
+
+
+//cleans data (automatically deltes special characters while the user is typing)
+function sanitize(string) {
+  const map = {
+      '&': '',
+      '<': '',
+      '>': '',
+      '"': '',
+      "'": '',
+      "/": '',
+  };
+  const reg = /[&<>"'/]/ig;
+  return string.replace(reg, (match)=>(map[match]));
 }
 
 const styles = StyleSheet.create({
@@ -163,4 +411,35 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
   },
+  addEventButton: {
+    backgroundColor: "#d3d3d3",
+    alignSelf: "flex-end",
+    height: 40,
+    borderRadius: 75,
+    marginRight: 10
+  },
+  addEventText: {
+    alignSelf: 'center',
+    fontSize: 20,
+  },
+  modalStyle: {
+    backgroundColor: 'white',
+    padding: 30,
+    margin: 30,
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 32,
+    marginBottom: 16,
+  },
+  errorText: {
+    backgroundColor: 'red',
+    padding: 10,
+  },
+  button: {
+    alignSelf: 'center',
+    marginTop: 10,
+  },
 });
+
+
