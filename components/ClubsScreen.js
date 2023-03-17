8d0b0e459547e19938e356d7cbbf2583b3226947
LaasYath <67027434+LaasYath@users.noreply.github.com>
2023-03-16 23:13:42 -0500

    Clubs Updated (To-Do: Sync front end to implement user's clubs changes & get filters to work)

--- components/ClubsScreen.js
+++ components/ClubsScreen.js
@@ -25,7 +25,7 @@ export const ClubsScreen = () => (
       <View style={{ flex: 1 }}><FilterMenu /></View>
     </View>
     <Divider bold={true} horizontalInset={true} />
-    <AllClubCards />
+    <AllClubCards filter=""/>
   </ScrollView>
 );
 
@@ -52,7 +52,7 @@ const UserClubCards = (props) => {
 
     const cards = cardData.map((step, move) => {
       return (
-        <ClubCard key={move} clubData={step} />
+        <ClubCardPersonal key={move} clubData={step} />
       );
     });
 
@@ -66,69 +66,6 @@ const UserClubCards = (props) => {
   // warning: unwatched promises, idk how to fix
   useEffect(() => {
     getCardData();
-  }, []);
-
-  return (
-    <View>
-      {cards}
-    </View>
-  );
-}
-
-const AllClubCards = () => {
-  // const cardsData = getAllClubCards();
-  const [cards, setCards] = useState(<ActivityIndicator 
-                                         animating={true} 
-                                         style={{ marginTop: 10, marginBottom: 10 }}
-  />);
-  let cardData = [];
- 
-  async function getAllClubCards() {
-    /* implement getting all clubs */
-    /* RETURN FORMAT: [{
-      clubTitle: str,
-      clubDescription: str,
-      clubCover: str,
-    }...]
-    (clubCover - can be URL or path (aka URI))
-    */  
-    let name = "", descrip = "", cover = "", x;
-
-    let search = global.school + "Clubs";
-    const Clubs = new Parse.Object.extend(search);
-    const queryClubs = new Parse.Query(Clubs);
-    const results = await queryClubs.find();
-    try {
-      for (const club of results) {
-        //retrieve info
-        name = club.get("name");
-        descrip = club.get("descrip");
-        cover = club.get("cover");
-
-        x = {
-          clubTitle: name,
-          clubDescription: descrip,
-          clubCover: cover,
-        }
-        //add club map to array
-        cardData.push(x);
-      }
-    } catch (error) {
-      console.error('Error while fetching Clubs', error);
-    }
-
-    //return cardData;
-    const cards = cardData.map((step, move) => {
-      return (
-        <ClubCard key={move} clubData={step} />
-      );
-    });
-
-    setCards(<View>{cards}</View>);
-  }
-
-  useEffect(() => {
-    getAllClubCards();
   }, []);
 
   return (
@@ -157,17 +94,17 @@ async function getUserClubCards() {
   //save object data to reponse
   const responseStudent = await objectStudent.save();
   //store user's clubs
-  const clubsList = responseStudent.get("clubs");
+  global.clubsList = responseStudent.get("clubs");
 
   let name = "";
   let descrip = "";
   let cover = "";
+  let id = "";
   let x;
 
   //gets club information
   let searchField = global.school + "Clubs";
-  const searchClub = Parse.Object.extend(searchField);
-  const query = new Parse.Query(searchClub);
+  let query = new Parse.Query(searchField);
   const results = await query.find();
 
   for (const club of results) {
@@ -176,10 +113,12 @@ async function getUserClubCards() {
       name = club.get("name");
       descrip = club.get("descrip");
       cover = club.get("cover");
+      id = club.id;
       x = {
         clubTitle: name,
         clubDescription: descrip,
         clubCover: cover,
+        clubID: id,
       }
       //add club map to array
       cardData.push(x);   
@@ -190,14 +129,87 @@ async function getUserClubCards() {
   // at the end, includes all user's clubs/maps
 }
 
-const ClubCard = (props) => {
+const AllClubCards = (props) => {
+  // const cardsData = getAllClubCards();
+  const [cards, setCards] = useState(<ActivityIndicator 
+                                         animating={true} 
+                                         style={{ marginTop: 10, marginBottom: 10 }}
+  />);
+  let cardData = [];
+ 
+  async function getAllClubCards() {
+    /* implement getting all clubs */
+    /* RETURN FORMAT: [{
+      clubTitle: str,
+      clubDescription: str,
+      clubCover: str,
+    }...]
+    (clubCover - can be URL or path (aka URI))
+    */  
+    let name = "", descrip = "", cover = "", x;
+
+    let search = global.school + "Clubs";
+    let queryClubs = new Parse.Query(search);
+
+    /*depending on seclected filter, the query is changed. tried reloading the component once the user clicks
+    the filter button with new properties
+    */
+    console.log("filter is: " + props.filter);
+    if (props.filter == "A-Z") {
+      queryClubs.ascending("name");
+    } else if (props.filter == "Z-A") {
+      queryClubs.descending("name");
+    }
+
+    const results = await queryClubs.find();
+    try {
+      for (const club of results) {
+        //retrieve info
+        name = club.get("name");
+        descrip = club.get("descrip");
+        cover = club.get("cover");
+
+        x = {
+          clubTitle: name,
+          clubDescription: descrip,
+          clubCover: cover,
+        }
+        //add club map to array
+        cardData.push(x);
+      }
+    } catch (error) {
+      console.error('Error while fetching Clubs', error);
+    }
+
+    //return cardData;
+    const cards = cardData.map((step, move) => {
+      return (
+        <ClubCardBrowse key={move} clubData={step} />
+      );
+    });
+
+    setCards(<View>{cards}</View>);
+  }
+
+  useEffect(() => {
+    getAllClubCards("");
+  }, []);
+
+  return (
+    <View>
+      {cards}
+    </View>
+  );
+}
+
+const ClubCardBrowse = (props) => {
   const clubInfo = props.clubData;
 
   return (
     <Card style={styles.card}>
       <Card.Title 
         title={clubInfo.clubTitle}
-        right={(props) => <ClubOptionsMenu {...props} clubData={clubInfo} />}
+        right={(props) => <ClubOptionsMenuBrowse {...props} clubData={clubInfo} />}
       />
       <Card.Content>
         <Paragraph>{clubInfo.clubDescription}</Paragraph>
@@ -207,7 +219,26 @@ const ClubCard = (props) => {
   );
 }
 
-const ClubOptionsMenu = (props) => {
+const ClubCardPersonal = (props) => {
+  const clubInfo = props.clubData;
+
+  return (
+    <Card style={styles.card}>
+      <Card.Title 
+        title={clubInfo.clubTitle}
+        right={(props) => <ClubOptionsMenuPersonal {...props} clubData={clubInfo} />}
+      />
+      <Card.Content>
+        <Paragraph>{clubInfo.clubDescription}</Paragraph>
+      </Card.Content>
+      <Card.Cover source={{ uri: clubInfo.clubCover }}/>
+    </Card>
+  );
+}
+
+//TO-DO: Once user leaves or joins club, resync front end to reflect user's change 
+
+const ClubOptionsMenuBrowse = (props) => {
   const clubInfo = props.clubData;
   const [visible, setVisible] = useState(false);
 
@@ -250,6 +281,117 @@ const ClubOptionsMenu = (props) => {
     takes user to instagram, shares most recent pic in camera roll (facebook has mo built in hooks/deep linking 
     to share pictures that aren't in the user's library or already posted)
     */
+    closeMenu();
+
+    const instagramURL = `instagram://library?AssetPath=""`;
+    return Linking.openURL(instagramURL);
+  
+  }
+
+  const savePhotoForInsta = async() => {
+    //image address for club poster
+    const uri = clubInfo.clubCover;
+
+    // gets permissions to user's camera roll
+    const permissionResponse = await MediaLibrary.requestPermissionsAsync();
+
+    //if permissions have been granted
+    if (permissionResponse.granted) {
+      //returns a null object, but event has successfully been completed
+      await MediaLibrary.saveToLibraryAsync(uri);
+      return true;
+    }
+
+  }
+
+  //adds an alert when photo is saved
+  const savePhoto = async() => {
+    const checkPermissions = savePhotoForInsta();
+    if(checkPermissions) {
+      alert("Poster has been saved to camera roll.");
+    }
+
+    closeMenu();
+  }
+
+  const joinClub = async() => {
+    console.log(clubInfo.clubTitle);
+    const userQuery = new Parse.Query(global.school);
+    const userObj = await userQuery.get(global.id);
+    let clubsList = userObj.get('clubs');
+    clubsList.push(clubInfo.clubTitle);
+    
+    try {
+      userObj.set('clubs', clubsList)
+      console.log("club joined");
+    } catch (error) {
+      console.log("Error: unable to join club => ", error);
+    }
+
+    closeMenu();
+  }
+
+  return (
+    <View style={styles.menuContainer}>
+      <Menu 
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={<IconButton icon="dots-vertical" onPress={openMenu}/>}
+      >
+        <Menu.Item onPress={joinClub} title='Join Club' />
+        <Menu.Item onPress={linkToInsta} title='Share to Instagram' />
+        <Menu.Item onPress={savePhoto} title='Save Poster' />
+        <Menu.Item onPress={share} title='Other' />
+      </Menu>
+    </View>
+  )
+}
+
+const ClubOptionsMenuPersonal = (props) => {
+  const clubInfo = props.clubData;
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+  const closeMenu = () => setVisible(false);
+
+  const share = async() => {
+    let uri = clubInfo.clubCover;
+
+    //better on ios (faster)
+    if (Sharing.isAvailableAsync()) {
+      const result = await Share.share(
+        {
+          //shares link to poster (base64 representation is way too long)
+          title: clubInfo.clubTitle + "\'s Club Poster",
+          url: uri,
+        },
+        {
+          excludedActivityTypes: [
+            //makes app laod faster by taking away airdrop option
+            'com.apple.UIK.activity.AirDrop',
+          ]
+        }
+      );
+
+      //success message if user shared an image
+      if (result.action === Share.sharedAction) {
+        alert("Shared successfully");
+      } 
+    }
+
+    closeMenu();
+  }
+
+
+  const linkToInsta = async() => {
+    //saves picture to user's camera roll
+    await savePhotoForInsta();
+    /*
+    takes user to instagram, shares most recent pic in camera roll (facebook has mo built in hooks/deep linking 
+    to share pictures that aren't in the user's library or already posted)
+    */
+    closeMenu();
+
     const instagramURL = `instagram://library?AssetPath=""`;
     return Linking.openURL(instagramURL);
   
@@ -276,6 +418,30 @@ const ClubOptionsMenu = (props) => {
     if(checkPermissions) {
       alert("Poster has been saved to camera roll.");
     }
+
+    closeMenu();
+  }
+
+  const leaveClub = async() => {
+    console.log(clubInfo.clubTitle);
+    // console.log(clubInfo.clubID);
+    // const toDel = await clubsQuery.get(clubInfo.clubID);
+    // console.log(toDel);
+    const userQuery = new Parse.Query(global.school);
+    const userObj = await userQuery.get(global.id);
+    let clubsList = userObj.get('clubs');
+    clubsList = clubsList.filter(function (club) {
+      return club != clubInfo.clubTitle;
+    });
+    
+    try {
+      userObj.set('clubs', clubsList)
+      console.log("club removed");
+    } catch (error) {
+      console.log("Error: unable to del club => ", error);
+    }
+
+    closeMenu();
   }
 
   return (
@@ -285,9 +451,10 @@ const ClubOptionsMenu = (props) => {
         onDismiss={closeMenu}
         anchor={<IconButton icon="dots-vertical" onPress={openMenu}/>}
       >
-        <Menu.Item onPress={share} title='Share' />
+        <Menu.Item onPress={leaveClub} title='Leave Club' />
+        <Menu.Item onPress={linkToInsta} title='Share to Instagram' />
         <Menu.Item onPress={savePhoto} title='Save Poster' />
-        <Menu.Item onPress={share} title='Options' />
+        <Menu.Item onPress={share} title='Other' />
       </Menu>
     </View>
   )
@@ -300,9 +467,18 @@ const FilterMenu = () => {
   const openMenu = () => setVisible(true);
   const closeMenu = () => setVisible(false);
 
-  const selectFilter = (filter) => {
-    setFilter(filter);
+  //TO-DO, implement filters by reloading page and sorting queries in asc/desc order
+  const selectFilterDesc = () => {
+    console.log("filter 1");
+    setFilter("A-Z");
+    <AllClubCards filter="A-Z"/>
     closeMenu();
+  }
+
+  const selectFilterAsc = () => {
+    setFilter("Z-A");
+    closeMenu();
+    <AllClubCards filter={"Z-A"}/>
   }
 
   return (
@@ -312,9 +488,8 @@ const FilterMenu = () => {
         onDismiss={closeMenu}
         anchor={<Button mode="outlined" onPress={openMenu}>{filter}</Button>}
       >
-        <Menu.Item onPress={() => {selectFilter('Filter 1')}} title='Filter 1' />
-        <Menu.Item onPress={() => {selectFilter('Filter 2')}} title='Filter 2' />
-        <Menu.Item onPress={() => {selectFilter('Filter 3')}} title='Filter 3' />
+        <Menu.Item onPress={() => {selectFilterDesc()}} title='A-Z' />
+        <Menu.Item onPress={() => {selectFilterAsc()}} title='Z-A' />
       </Menu>
     </View>
   )
